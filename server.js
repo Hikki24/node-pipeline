115ad0f82a07c350a9895705f780f2e0e49d8c09
Vladislav <vladislavkurochkin7@gmail.com>
2026-04-26 18:42:25 +0300

    Update app to v2.0 for Watchtower test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>?? Node.js App v1.0</h1><p>Deployed via GitHub Actions</p>');
+  res.send('<h1>? Node.js App v2.0</h1><p>Auto-updated by Watchtower!</p>');
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
